cc43b63daae90823a9298cd310f653907eb8bbd9
minson123-github <minson123.mail@gmail.com>
2021-08-01 18:32:46 +0800

    set multi-GPU training to ddp mode and add auto_find_lr

--- bert_relevant_classifier/run.py
+++ bert_relevant_classifier/run.py
@@ -3,6 +3,7 @@ from config import get_args
 from model import relevantClassifier
 from data_process import get_train_dataloader
 from pytorch_lightning import Trainer, seed_everything, Callback
+from fairscale.nn import checkpoint_wrapper, auto_wrap, wrap
 
 class CheckpointEveryNSteps(Callback):
 	def __init__(self, save_freq, save_dir):
@@ -20,8 +21,6 @@ class CheckpointEveryNSteps(Callback):
 
 def train(args):
 	seed_everything(args['seed'], workers=True)
-	model = relevantClassifier(args)
-	train_dataloader, eval_dataloader = get_train_dataloader(args)
 
 	save_path = os.path.join(args['model_dir'], 'model')
 	if not os.path.exists(save_path):
@@ -32,16 +31,31 @@ def train(args):
 		callbacks = [CheckpointEveryNSteps(args['saving_steps'], os.path.join(args['model_dir'], 'checkpoints'))]
 		if not os.path.exists(os.path.join(args['model_dir'], 'checkpoints')):
 			os.makedirs(os.path.join(args['model_dir'], 'checkpoints'))
+	
+	model = relevantClassifier(args)
 
+	train_dataloader, eval_dataloader = get_train_dataloader(args)
 	trainer = Trainer(
 					deterministic=True, 
 					gpus=args['n_gpu'], 
 					default_root_dir=save_path, 
 					max_epochs=args['n_epoch'], 
 					num_nodes=1, 
-					accelerator="dp", 
-					callbacks = callbacks
+					# precision=16, 
+					accelerator="ddp", 
+					plugins="ddp_sharded", 
+					callbacks = callbacks, 
 				)
+	
+	'''
+	lr_finder = trainer.tuner.lr_find(model, train_dataloaders=train_dataloader)
+	print(lr_finder.results)
+	new_lr = lr_finder.suggestion()
+	model.hparams.lr = new_lr
+	'''
+
+	print('The learning rate be used for training is: ', model.lr, flush=True)
+
 	print('start to training model.', flush=True)
 	if args['eval_mode']:
 		trainer.fit(model, train_dataloader, eval_dataloader)
