0a9efe9f92feb6af9a8922fdf5d9d957a83837b8
minson123-github <minson123.mail@gmail.com>
2021-08-01 18:32:14 +0800

    change lr setting

--- bert_relevant_classifier/config.py
+++ bert_relevant_classifier/config.py
@@ -14,7 +14,7 @@ def get_args():
 	parser.add_argument('--eval_mode', type=int, default=0, help='Whether to split evaluate dataset for evaluating model performance.')
 	parser.add_argument('--eval_ratio', type=float, default=0, help='The ratio of evaluate dataset in all dataset.')
 	parser.add_argument('--n_gpu', type=int, default=1, help='Number of GPU be used in training.')
-	parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate for training.')
+	parser.add_argument('--lr', type=float, default=1e-5, help='Learning rate for training.')
 	parser.add_argument('--n_epoch', type=int, default=1, help='Number of epoch for training.')
 	parser.add_argument('--saving_steps', type=int, help='Number of step for saving model.')
 	parser.add_argument('--mode', type=str, help='Mode: train or test')
